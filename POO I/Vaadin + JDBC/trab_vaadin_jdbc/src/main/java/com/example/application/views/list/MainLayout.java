73bd9bb1cc6c1fcee462b628a43b565da8d54455
Yduan Rech Effting <105087789+yduanrech@users.noreply.github.com>
2023-09-28 18:16:40 -0300

    Add files via upload

---
+++ POO I/Vaadin + JDBC/trab_vaadin_jdbc/src/main/java/com/example/application/views/list/MainLayout.java
@@ -0,0 +1,51 @@
+package com.example.application.views.list;
+
+import com.example.application.views.list.ListView;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.HighlightConditions;
+import com.vaadin.flow.router.RouterLink;
+import com.vaadin.flow.theme.lumo.LumoUtility;
+
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.router.Route;
+
+@Route(value = "list", layout=MainLayout.class)
+public class MainLayout extends AppLayout { 
+
+    public MainLayout() {
+        createHeader();
+        createDrawer();
+    }
+
+    private void createHeader() {
+        H1 logo = new H1("CRM");
+        logo.addClassNames(
+            LumoUtility.FontSize.LARGE, 
+            LumoUtility.Margin.MEDIUM);
+
+        var header = new HorizontalLayout(new DrawerToggle(), logo ); 
+
+        header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER); 
+        header.setWidthFull();
+        header.addClassNames(
+            LumoUtility.Padding.Vertical.NONE,
+            LumoUtility.Padding.Horizontal.MEDIUM);
+
+        addToNavbar(header); 
+
+    }
+
+    private void createDrawer() {
+        addToDrawer(new VerticalLayout( 
+                new RouterLink("Cadastro", ListView.class)
+                /*new RouterLink("Dashboard", DashboardView.class)*/
+        ));
+    }
+
+
+}
